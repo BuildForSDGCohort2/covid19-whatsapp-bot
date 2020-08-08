68f46ef5dbd264ef5c834d071e9bcfc3d30f37ec
repl.it user <replituser@example.com>
2020-08-08 11:32:34 +0000

    Added covid api to pull data from per specified country

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var app = express();
 var request = require('request');
 const bodyParser = require('body-parser');
 
+const twilio = require('twilio')
 
-const accountSid = AC85aa1607823114fab149a3e8c26ad443;
-const authToken = f01014fa0fc7ac2723e6c81515038c07;
+require('dotenv');
+const accountSid = "AC85aa1607823114fab149a3e8c26ad443";
+const authToken = "f01014fa0fc7ac2723e6c81515038c07" ;
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
@@ -15,30 +17,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.post('/incoming', (req, res) => {
+  
   const twiml = new MessagingResponse();
   if(req.body.Body.toLowerCase().trim()!="hi" && req.body.Body.toLowerCase().trim()!="hello" && req.body.Body.toLowerCase().trim()!="test" && req.body.Body.toLowerCase().trim()!="help"){
-  request('https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q='+req.body.Body, function (error, response, body) {
+  request('https://coronavirus-19-api.herokuapp.com/countries/'+req.body.Body, function (error, response, body) {
     body = JSON.parse(body)
-    console.log('body:', body["Abstract"]);
+    console.log(body);
     
-    if(body["Abstract"] == ""){
-	    body["Abstract"]= body["RelatedTopics"][0]["Text"]
-	  }
-    
-    var msg = twiml.message(`*`+body["Heading"]+`*
-`+body["Abstract"]);
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-  });
-  }
+   if (body == '') {
+      body = body
+    }
+
+    const msg = twiml.message(body['country'] + '\n\n' + body['todayCases']+ '\n\n' +body['todayDeaths']+ '\n\n' +body['cases']+ '\n\n' +body['active']+ '\n\n' +body['deaths']+ '\n\n' +body['recovered']+ '\n\n' +body['critical'])
+    res.writeHead(200, { 'Content-Type': 'text/xml' })
+    res.end(twiml.toString())})}
   else{
     var msg = twiml.message(`*Hey 👋*
-I am a bot which summarizes WikiPedia pages to help you find quick information, right within WhatsApp.
-Try it out - send me anything you want to know about`)
-    res.writeHead(200, {'Content-Type': 'text/xml'});
+.
+Try it out - send me anything you want to know about about covid statistics in your country`)
+    res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(twiml.toString());
   }
-  
+  console.log(req.body)
 });
 
 app.post('/check', function(req, res) {
