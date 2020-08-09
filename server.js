789aa9a6ce97103b506cc3c43ad7e4af0ec46660
Mulubwa17 <mulubwac@gmail.com>
2020-08-09 12:03:03 +0200

    Added env varaibles

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.post('/incoming', (req, res) => {
-  
+  var date =Date.now();
   const twiml = new MessagingResponse();
   if(req.body.Body.toLowerCase().trim()!="hi" && req.body.Body.toLowerCase().trim()!="hello" && req.body.Body.toLowerCase().trim()!="test" && req.body.Body.toLowerCase().trim()!="help"){
   request('https://coronavirus-19-api.herokuapp.com/countries/'+req.body.Body, function (error, response, body) {
@@ -28,16 +28,16 @@ app.post('/incoming', (req, res) => {
       body = body
     }
 
-    const msg = twiml.message( body['country'] +  '\n\n'+'Cases Today : ' + body['todayCases']+ '\n\n'+'Deaths Today : ' +body['todayDeaths']+ '\n\n'+'Total Cases : ' +body['cases']+ '\n\n'+'Active Cases : ' +body['active']+ '\n\n'+'Total Deaths : ' +body['deaths']+ '\n\n'+'Total Recovered : ' +body['recovered']+ '\n\n'+'Total critical : ' +body['critical']+
+    const msg = twiml.message( body['country'] + date + '\n\n'+'Cases Today : ' + body['todayCases']+ '\n\n'+'Deaths Today : ' +body['todayDeaths']+ '\n\n'+'Total Cases : ' +body['cases']+ '\n\n'+'Active Cases : ' +body['active']+ '\n\n'+'Total Deaths : ' +body['deaths']+ '\n\n'+'Total Recovered : ' +body['recovered']+ '\n\n'+'Total critical : ' +body['critical']+
 '\n\n' + 'Created by Mulubwa Chungu'+
-'\n\n' + 'linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/'+'\n\n' + 'gihub: https://github.com/Mulubwa17')
+'\n\n' + 'linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/'+'\n\n' + 'github: https://github.com/Mulubwa17')
     res.writeHead(200, { 'Content-Type': 'text/xml' })
     res.end(twiml.toString())})}
   else{
     var msg = twiml.message(`*Hey 👋*
 Try it out - send me any country name to get its covid statistics`+
 '\n\n' + 'Bot Created by Mulubwa Chungu'+
-'\n\n' + 'linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/'+'\n\n' + 'gihub: https://github.com/Mulubwa17')
+'\n\n' + 'linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/'+'\n\n' + 'github: https://github.com/Mulubwa17')
     res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(twiml.toString());
   }
