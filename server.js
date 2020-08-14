1be5abb61e8a47f4d56cc1de9375af281aa2d1b8
Mulubwa17 <mulubwac@gmail.com>
2020-08-14 11:17:27 +0200

    New auth ids

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const request = require("request");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
-const twilio = require("twilio");
 dotenv.config();
 const app = express();
-const accountSid = "AC85aa1607823114fab149a3e8c26ad443";
-const authToken = "f01014fa0fc7ac2723e6c81515038c07";
+const accountSid = process.env.SID;
+const authToken = process.env.AUTH;
 const client = require("twilio")(accountSid, authToken);
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
@@ -26,7 +25,8 @@ app.post("/incoming", (req, res) => {
     req.body.Body.toLowerCase().trim() != "hi" &&
     req.body.Body.toLowerCase().trim() != "hello" &&
     req.body.Body.toLowerCase().trim() != "test" &&
-    req.body.Body.toLowerCase().trim() != "help"
+    req.body.Body.toLowerCase().trim() != "help" &&
+    req.body.Body.toLowerCase().trim() != "Undefined"
   ) {
     request(
       "https://disease.sh/v3/covid-19/countries/" + req.body.Body,
@@ -40,40 +40,40 @@ app.post("/incoming", (req, res) => {
 
         const msg = twiml.message(
           body["country"] +
-            "  " +
-            // "(" +
-            // date +
-            // ")" +
-            "\n\n" +
-            "Cases Today : " +
-            body["todayCases"] +
-            "\n\n" +
-            "Deaths Today : " +
-            body["todayDeaths"] +
-            "\n\n" +
-            "Total Cases : " +
-            body["cases"] +
-            "\n\n" +
-            "Active Cases : " +
-            body["active"] +
-            "\n\n" +
-            "Total Deaths : " +
-            body["deaths"] +
-            "\n\n" +
-            "Total Recovered : " +
-            body["recovered"] +
-            "\n\n" +
-            "Total critical : " +
-            body["critical"] +
-            "\n\n" +
-            "Total tests : " +
-            body["tests"] +
-            "\n\n" +
-            "Created by Mulubwa Chungu" +
-            "\n\n" +
-            "linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/" +
-            "\n\n" +
-            "github: https://github.com/Mulubwa17"
+          "  " +
+          // "(" +
+          // date +
+          // ")" +
+          "\n\n" +
+          "Cases Today : " +
+          body["todayCases"] +
+          "\n\n" +
+          "Deaths Today : " +
+          body["todayDeaths"] +
+          "\n\n" +
+          "Total Cases : " +
+          body["cases"] +
+          "\n\n" +
+          "Active Cases : " +
+          body["active"] +
+          "\n\n" +
+          "Total Deaths : " +
+          body["deaths"] +
+          "\n\n" +
+          "Total Recovered : " +
+          body["recovered"] +
+          "\n\n" +
+          "Total critical : " +
+          body["critical"] +
+          "\n\n" +
+          "Total tests : " +
+          body["tests"] +
+          "\n\n" +
+          "Created by Mulubwa Chungu" +
+          "\n\n" +
+          "linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/" +
+          "\n\n" +
+          "github: https://github.com/Mulubwa17"
         );
         res.writeHead(200, {
           "Content-Type": "text/xml",
@@ -82,17 +82,18 @@ app.post("/incoming", (req, res) => {
       }
     );
   } else {
+
     var msg = twiml.message(
       `*Hey 👋*
 Seems you mistyped there,` +
-        `\n\n` +
-        `- Send me any country name to get its covid statistics` +
-        "\n\n" +
-        "Bot Created by Mulubwa Chungu" +
-        "\n\n" +
-        "linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/" +
-        "\n\n" +
-        "github: https://github.com/Mulubwa17"
+      `\n\n` +
+      `- Send me any country name to get its covid statistics` +
+      "\n\n" +
+      "Bot Created by Mulubwa Chungu" +
+      "\n\n" +
+      "linkedIn :https://www.linkedin.com/in/mulubwa-chungu-39748098/" +
+      "\n\n" +
+      "github: https://github.com/Mulubwa17"
     );
     res.writeHead(200, {
       "Content-Type": "text/html",
